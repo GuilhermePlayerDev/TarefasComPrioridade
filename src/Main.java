e558c8af0c3a802d2aeccb208ddf52c7037d5f05
Guilherme Guimarães <166334818+GuilhermePlayerDev@users.noreply.github.com>
2026-04-03 00:42:33 -0300

    1° commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/* 9. Lista de Tarefas com Prioridade: Crie uma classe Tarefa com atributos para
+descrição e prioridade (use um Enum para as prioridades: ALTA, MEDIA, BAIXA).
+Crie uma classe ListaDeTarefas que armazene tarefas e as exiba ordenadas por
+prioridade */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
